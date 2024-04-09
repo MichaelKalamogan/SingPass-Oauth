800b7dd61d4a5d74c1d2d0f12eae7750378f1bcd
Michael Kalamogan <michael@cakedefi.com>
2024-04-09 11:41:52 +0800

    fix: typos

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get("/auth", (req, res) => {
   if (process.env.AUTH0_CLIENT_ID !== req.query.client_id) {
     return res.send(401, "invalid client_id");
   }
-  var url = `https://id.singpass.gov.sg/${req.url}&client_id=${process.env.SINGPASS_CLIENT_ID}&state=${req.query.state}&nonce=${req.query.code_challenge}`;
+  var url = `https://id.singpass.gov.sg${req.url}&client_id=${process.env.SINGPASS_CLIENT_ID}&state=${req.query.state}&nonce=${req.query.code_challenge}`;
   res.redirect(url);
 });
 
